c1574590d8863acfb3a00a8b313f3d706f8bcb25
Grigory <grtkachenko@gmail.com>
2013-11-09 11:08:41 +0400

    Minor changes applied

--- include/cg/triangulation/delaunay.h
+++ include/cg/triangulation/delaunay.h
@@ -7,7 +7,7 @@
 #include <cg/io/segment.h>
 #include <set>
 #include <map>
-#include <cg/common/values.h>
+#include <boost/optional.hpp>
 
 namespace cg
 {
@@ -15,27 +15,26 @@ namespace cg
    template <class Scalar>
    struct edge_less {
       bool operator() (segment_2t<Scalar> const & a, segment_2t<Scalar> const & b) const {
-         return a[0] > b[0] || (a[0] == b[0] && a[1] > b[1]);
+         return a[0] < b[0] || (a[0] == b[0] && a[1] < b[1]);
       }
    };
 
    template <class Scalar>
-   void update_edge(segment_2t<Scalar> & seg, std::set<segment_2t<Scalar>, edge_less<Scalar> > & border) {
+   void update_edge(segment_2t<Scalar> & seg, point_2t<Scalar> & opposite_point,
+                    std::set<segment_2t<Scalar>, edge_less<Scalar> > & border,
+                    std::map<segment_2t<Scalar>, point_2t<Scalar>, edge_less<Scalar> > & point_by_edge) {
       if (border.count(seg) != 0) {
          border.erase(seg);
       } else {
-         auto tmp = seg[0];
-         seg[0] = seg[1];
-         seg[1] = tmp;
+         std::swap(seg[0], seg[1]);
          border.insert(seg);
+         point_by_edge[seg] = opposite_point;
       }
    }
 
    template <class Scalar>
    point_2t<Scalar> mid_ort_intersect(point_2t<Scalar> & a, point_2t<Scalar> & b, point_2t<Scalar> & c) {
       vector_2t<Scalar> q1(b.x - a.x, b.y - a.y), q2(c.x - b.x, c.y - b.y);
-      if ((q1 ^ q2) == 0) return point_2(0, 0);
-
       point_2t<Scalar> m1((a.x + b.x) / 2, (a.y + b.y) / 2), m2((c.x + b.x) / 2, (c.y + b.y) / 2);
       Scalar a1 = q1.x, b1 = q1.y, c1 = -(q1.x * m1.x + q1.y * m1.y), a2 = q2.x, b2 = q2.y, c2 = -(q2.x * m2.x + q2.y * m2.y);
       return point_2t<Scalar>((b1 * c2 - b2 * c1) / (b2 * a1 - b1 * a2), (a1 * c2 - a2 * c1) / (-b2 * a1 + b1 * a2));
@@ -75,25 +74,22 @@ namespace cg
       while (!border.empty()) {
          Segment e = *border.begin();
          border.erase(border.begin());
-         Point * best_point = nullptr;
+         boost::optional<Point> best_point;
          for (int i = 0; i < points.size(); i++) {
             Point p = points[i];
             if (e[0] != p && e[1] != p && cg::orientation(e[0], e[1], p) == cg::CG_RIGHT) {
                if ((point_by_edge.count(e) == 0 || less_ort(e, point_by_edge[e], p)) &&
-                       (best_point == nullptr || less_ort(e, p, *best_point))) {
-                  best_point = &points[i];
+                       (!best_point|| less_ort(e, p, *best_point))) {
+                  best_point =  points[i];
                }
             }
          }
 
          if (best_point) {
-            Point s(e[0]), f(e[1]);
-            res.push_back(Triangle(s, f, *best_point));
-            Segment first(f, *best_point), second(*best_point, s);
-            update_edge(first, border);
-            update_edge(second, border);
-            point_by_edge[first] = s;
-            point_by_edge[second] = f;
+            res.push_back(Triangle(e[0], e[1], *best_point));
+            Segment first(e[1], *best_point), second(*best_point, e[0]);
+            update_edge(first, e[0], border, point_by_edge);
+            update_edge(second, e[1], border, point_by_edge);
          }
       }
 
